48e992c4fab48aa9f6b3fdfb6cca58230ea2af23
iouhase <emilowithc@gmail.com>
2026-02-20 23:11:26 +0100

    did stuff

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,79 @@
-use std::{io::{Result, Write}, process::ExitStatus, fs::File};
+use std::{io};
+
+use rustmediaplayer::{MusicPlayer, error_helper, console_clear};
 
 fn main() {
-    console_clear();
+    let mut music_player: MusicPlayer = MusicPlayer::new().expect("falied to create music player");
+    let mut current_song:Option<usize> = None;
 
-    const INTERNET_CONNECTION: bool = true;
-
-    if !INTERNET_CONNECTION {
-        let no_internet_warn = "WARN: device has no internetconnection";
-        println!("{}", no_internet_warn);
-        log_action(no_internet_warn.to_string()).expect("ERROR: could not log");
-    }
-    
     loop {
-        
-    }
-}
+        if let Err(err) = console_clear() {
+            error_helper(format!("{err}"));
+        }
 
-fn log_action(message: String) -> Result<()> {
-    let logfile = "log.txt";
-    let mut file: File = std::fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open(logfile)?;
-    file.write_all((message + "\n").as_bytes())?;
-    Ok(())
-}
+        let song_list = match music_player.song_list() {
+            Err(err) => {
+                error_helper(format!("{err}"));
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+                continue;
+            }
+            Ok(list) => list,
+        };
 
-fn handle_console_clear_error(result: Result<ExitStatus>) {
-    match result {
-        Ok(_) => {
-            log_action("INFO: console has been cleared".to_string()).expect("ERROR: could not log");
-        },
-        Err(err) => {
-            log_action("ERROR: console could not be cleared".to_string()).expect("ERROR: could not log");
-            println!("ERROR: console could not be cleared: {}", err);
+        println!("song list:");
+        for (index, song) in song_list.iter().enumerate() {
+            if current_song == Some(index) {
+                print!("\x1b[1;31m")
+            }
+            println!(
+                "{index}) {}",
+                song.file_name()
+                    .unwrap_or_default()
+                    .to_str()
+                    .unwrap_or_default()
+            );
+            print!("\x1b[0m");
+        }
+
+        println!("\nnum) type number of song to play");
+        println!("p) to pause song");
+        println!("r) to resume playing song");
+        println!("q) to exit app");
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Err(err) => {
+                error_helper(format!("{err}"));
+            }
+            Ok(_) => {
+                match input.trim().to_ascii_lowercase().as_str(){
+                    "p" => {
+                        music_player.pause();
+                        continue;
+                    },
+                    "r" => {
+                        music_player.resume();
+                        continue;
+                    },
+                    "q" => {
+                        break;
+                    }
+                    _ => {}
+                }
+                match input.trim().parse::<usize>() {
+                    Err(_) => {error_helper(format!("please choose a number"));},
+                    Ok(num) => {
+                        let song = match song_list.get(num) {
+                            Some(s) => {s},
+                            None => {error_helper(format!("song not in list")); continue;},
+                        };
+                        current_song = Some(num);
+                        if let Err(err) = music_player.play_song(song) {
+                            error_helper(format!("{err}"));
+                        }
+                    }
+                }
+            }
         }
     }
-}
-
-fn console_clear() {
-    handle_console_clear_error(
-        if cfg!(windows) {
-            log_action("INFO: clearing windows console".to_string()).expect("ERROR: could not log");
-            std::process::Command::new("cmd").args(&["/C", "cls"]).status()
-        } else {
-            log_action("INFO: clearing unix console".to_string()).expect("ERROR: could not log");
-            std::process::Command::new("clear").status()
-        }
-    );
 }
